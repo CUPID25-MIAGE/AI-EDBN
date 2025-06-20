78fa20d3a7a3dd8629fce52b4125de9888b6d730
raymond <rerezheng@gmail.com>
2025-06-20 19:07:19 +0200

    add helper, need to change the name

---
+++ helper.py
@@ -0,0 +1,13 @@
+def filter_check(data_to_filter): #return false if the result should be filtered out
+    filter_event_to_block = ["doorOpened","doorClosed", "sunUp", "sunDown", "nicolasDetected", "nicolasNotDected"]
+    return not (data_to_filter in filter_event_to_block)
+
+
+def get_time_stamp_case_id():
+    import datetime
+    now = datetime.datetime.now()
+    now_minus_3h = now - datetime.timedelta(hours=4) #reset du case id at 3 pm to next day
+    return now_minus_3h.strftime("%Y%m%d")
+
+
+print(get_time_stamp_case_id())
